aa0f48e3add339769ad292c2b671a13bc23f9fa9
Gino Dera <ginodera@live.nl>
2024-11-17 13:54:24 +0100

    feat: initial commit

---
+++ Client/ClientProgram.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client;
+
+internal static class ClientProgram
+{
+    private static async Task Main()
+    {
+        var buffer = new byte[1024];
+
+        var client = new TcpClient();
+        await client.ConnectAsync(IPAddress.Loopback, 8080);
+
+        while (true)
+        {
+            const string message = "The quick brown fox jumps over the lazy dog.";
+            var encoded = Encoding.Default.GetBytes(message);
+            Array.Copy(encoded, 0, buffer, 0, encoded.Length);
+
+            var socketAsyncEventArgs = new SocketAsyncEventArgs();
+            socketAsyncEventArgs.SendPacketsElements =
+            [
+                new SendPacketsElement(buffer, 0, encoded.Length)
+            ];
+
+            client.Client.SendPacketsAsync(socketAsyncEventArgs);
+
+            await Task.Delay(10);
+        }
+    }
+}
---
+++ Server/ServerProgram.cs
@@ -0,0 +1,93 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server;
+
+public class Connection
+{
+    public required TcpClient Client { get; init; }
+    public required Socket Socket { get; init; }
+
+    public int TotalPacketCount { get; set; }
+    public long TotalPacketSize { get; set; }
+
+    public override int GetHashCode()
+    {
+        return Client.Client.RemoteEndPoint!.GetHashCode();
+    }
+}
+
+public readonly struct ConnectionPacket(Connection connection, byte[] data)
+{
+    public readonly Connection connection = connection;
+    public readonly byte[] data = data;
+}
+
+internal static class ServerProgram
+{
+    private static readonly HashSet<Connection> _connections = [];
+
+    private static async Task Main()
+    {
+        _ = Task.Run(async () =>
+        {
+            while (true)
+            {
+                await Task.Delay(1000);
+                Console.Clear();
+                Console.WriteLine("-- Connection information --");
+                Console.WriteLine($"Active connections: {_connections.Count}");
+                Console.WriteLine($"Total packet count: {_connections.Sum(c => c.TotalPacketCount)}");
+                Console.WriteLine($"Total packet size: {_connections.Sum(c => c.TotalPacketSize)}");
+            }
+        });
+
+        var listener = new TcpListener(IPAddress.Any, 8080);
+        listener.Start();
+
+        while (true)
+        {
+            var client = await listener.AcceptTcpClientAsync();
+            var connection = new Connection
+            {
+                Client = client,
+                Socket = client.Client
+            };
+
+            _connections.Add(connection);
+            _ = HandleClientConnection(connection);
+        }
+    }
+
+    private static async Task HandleClientConnection(Connection connection)
+    {
+        try
+        {
+            var buffer = new byte[1024];
+
+            while (true)
+            {
+                var length = await connection.Socket.ReceiveAsync(buffer);
+                connection.TotalPacketCount += 1;
+                connection.TotalPacketSize += length;
+
+                var data = buffer[..length];
+
+                ThreadPool.QueueUserWorkItem(
+                    callBack: HandlePacket,
+                    state: new ConnectionPacket(connection, data),
+                    preferLocal: true
+                );
+            }
+        }
+        catch (SocketException)
+        {
+            _connections.Remove(connection);
+        }
+    }
+
+    private static void HandlePacket(ConnectionPacket packet)
+    {
+    }
+}
